ec40c13b313a9f06b2b4113cb228b52f577e7112
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-09-30 18:32:56 -0300

    feat: add route handler for events

---
+++ src/app/api/events/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+import { getEvents } from "~/server/models/Events";
+
+export async function GET() {
+  const events = await getEvents();
+  return NextResponse.json(events);
+}
