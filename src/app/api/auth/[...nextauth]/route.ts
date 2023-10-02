ac0f03eb8f8f562006540146a52bf2ccf69868bc
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-10-01 21:02:07 -0300

    feat: redefine schemas

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,54 @@
-import NextAuth from "next-auth";
+import NextAuth, { type AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
+const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      // profile(profile) {
+      //   return {
+      //     id: profile.id,
+      //     name: profile.name,
+      //     email: profile.email,
+      //     image: profile.image,
+      //     role: profile.role ?? "user"
+      //   };
+      // },
     }),
   ],
-});
+  // callbacks: {
+  //   async jwt({ token, user }) {
+  //     if (user) {
+  //       token.role = user.role;
+  //     }
+  //     return token;
+  //   },
+
+  //   async session({ session, token }) {
+  //     if (session.user) {
+  //       session.user.role = token.role;
+  //     }
+  //     return session;
+  //   },
+  // },
+  pages: {
+    signIn: "/signin",
+    // signOut: "/auth/signout",
+    // error: "/auth/error",
+    // verifyRequest: "/auth/verify-request",
+    // newUser: "/auth/new-user",
+  },
+  adapter: PrismaAdapter(prisma),
+  // session: {
+  //   strategy: "jwt",
+  // },
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
