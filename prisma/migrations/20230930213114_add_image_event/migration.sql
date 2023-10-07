01103e4c575b219e45f0aecc67fcb053e979bbc7
Joaquin Arlettaz <87623271+Joadevy@users.noreply.github.com>
2023-10-07 17:34:57 -0300

    Delete prisma directory

--- prisma/migrations/20230930213114_add_image_event/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `imageUrl` to the `Event` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "imageUrl" TEXT NOT NULL;
