f92f936fc0a98fcd6d7cb9f144cbdfd9cf466ab1
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-10-10 01:53:44 -0300

    fix:enable all origins

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,41 +1,69 @@
+// import { NextResponse } from "next/server";
+
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "http://localhost:3001",
+//   "https://weekendless.vercel.app/",
+//   "https://weekendless.vercel.app",
+// ];
+
+// export const corsHeaders = {
+//   "Access-Control-Allow-Origin": "",
+//   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+//   "Access-Control-Allow-Headers": "Content-Type, Authorization",
+// };
+
+// export function middleware(request: Request) {
+//   // const origin = request.headers.get("origin") ?? "";
+//   const response = NextResponse.next();
+
+//   // if (allowedOrigins.includes(origin) || origin.includes("weekendless")) {
+//   //   response.headers.set("Access-Control-Allow-Origin", origin);
+//   // }
+
+//   if (request.method === "OPTIONS") {
+//     response.headers.set("Access-Control-Allow-Origin", "*");
+//     response.headers.set(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, PUT, DELETE, OPTIONS",
+//     );
+//     response.headers.set(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type, Authorization",
+//     );
+//   }
+
+//   return response;
+// }
+import type { NextRequest } from "next/server";
+
 import { NextResponse } from "next/server";
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "https://weekendless.vercel.app/",
-  "https://weekendless.vercel.app",
-];
+export function middleware(request: NextRequest) {
+  // Clone the request headers and set a new header `x-hello-from-middleware1`
+  const requestHeaders = new Headers(request.headers);
 
-export const corsHeaders = {
-  "Access-Control-Allow-Origin": "",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
+  requestHeaders.set("x-hello-from-middleware1", "hello");
+  requestHeaders.set("Access-Control-Allow-Origin", "*");
+  requestHeaders.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS",
+  );
+  requestHeaders.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization",
+  );
 
-export function middleware(request: Request) {
-  // const origin = request.headers.get("origin") ?? "";
-  const response = NextResponse.next();
+  // You can also set request headers in NextResponse.rewrite
+  const response = NextResponse.next({
+    request: {
+      // New request headers
+      headers: requestHeaders,
+    },
+  });
 
-  // if (allowedOrigins.includes(origin) || origin.includes("weekendless")) {
-  //   response.headers.set("Access-Control-Allow-Origin", origin);
-  // }
-
-  if (request.method === "OPTIONS") {
-    response.headers.set("Access-Control-Allow-Origin", "*");
-    response.headers.set(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS",
-    );
-    response.headers.set(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization",
-    );
-  }
+  // Set a new response header `x-hello-from-middleware2`
+  response.headers.set("x-hello-from-middleware2", "hello");
 
   return response;
 }
-
-export const config = {
-  matcher: "/api/:path*",
-};
