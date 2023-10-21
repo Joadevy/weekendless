99c23735ef7a49a1989b550c13785aa1bc281bb5
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-10-21 13:15:06 -0300

    refactor: make the prisma connection a singleton

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export const db = prisma;
