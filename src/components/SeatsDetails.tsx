e44bcc08385f5d4013d125ebb9240297d41e1777
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-10-28 17:52:52 -0300

    fix: remove unused functions and state

--- src/components/SeatsDetails.tsx
+++ src/components/SeatsDetails.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { type TypeSeat } from "@prisma/client";
 
 import MakeReservation from "./MakeReservation";
@@ -18,15 +17,7 @@ type Props = {
 };
 
 const SeatsDetails = ({ seats }: Props) => {
-  const [clientSeats, setClientSeats] = useState(seats);
-
-  const handleReservation = (seatId: number) => {
-    const updatedSeats = clientSeats.filter((seat) => seat.id !== seatId);
-
-    setClientSeats(updatedSeats);
-  };
-
-  if (clientSeats.length === 0) {
+  if (!seats || seats.length === 0) {
     return <p className="text-slate-400 italic">No seats available</p>;
   }
 
@@ -34,7 +25,7 @@ const SeatsDetails = ({ seats }: Props) => {
     <>
       <h2>Book your ticket:</h2>
       <ul className="flex gap-4 flex-wrap mt-1">
-        {clientSeats.map((seat) => (
+        {seats.map((seat) => (
           <li key={seat.id} className="bg-slate-100 p-2 rounded-sm shadow-sm">
             <HoverCard>
               <HoverCardTrigger className="relative flex flex-col justify-center items-center">
@@ -46,10 +37,7 @@ const SeatsDetails = ({ seats }: Props) => {
                     currency: "ARS",
                   })}
                 </p>
-                <MakeReservation
-                  handleReservation={handleReservation}
-                  seat={seat}
-                />
+                <MakeReservation seat={seat} />
               </HoverCardTrigger>
             </HoverCard>
           </li>
